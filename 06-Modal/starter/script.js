72ed971db4892e38e131b202aa6163d2373c5f26
bradage <bradage.merrill@gmail.com>
2021-02-17 22:04:34 -0700

    modal completed

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,8 +4,26 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 
-console.log(btnShowModal);
-
-for (let i = 0; i > btnShowModal.length; i++) {
-    console.log(btnShowModal[i]);
+function openModal() {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 }
+
+
+for (let i = 0; i < btnShowModal.length; i++) {
+    btnShowModal[i].addEventListener('click', openModal);
+}
+
+function closeModal() {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+    }
+});
